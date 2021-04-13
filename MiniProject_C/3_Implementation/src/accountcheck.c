5d0a94cb15a7dbc1c976db1955d50e8ac016e452
udupaAnusha <55906876+udupaAnusha@users.noreply.github.com>
2021-04-13 12:10:02 +0530

    Update accountcheck.c

--- MiniProject_C/3_Implementation/src/accountcheck.c
+++ MiniProject_C/3_Implementation/src/accountcheck.c
@@ -1,1 +1,36 @@
+// Function to check if the account
+// already exists or not
+void account_check()
+{
+	for (i = 0; i < 100; i++) {
+		// Check whether the email and
+		// password are already matched
+		// with existed account
+		if (!(strcmp(temp_email,
+					s[i].email)
+			&& strcmp(temp_password1,
+						s[i].password))) {
+			printf("\n\nAccount Already");
+			printf("Existed. Please"
+				" Login !!\n\n");
+			main();
+			break;
+		}
+	}
 
+	// If account does not already
+	// existed, it creates a new
+	// one with new inputs
+	if (i == 100) {
+		strcpy(s[j].uname, temp_name);
+		s[j].age = temp_age;
+		strcpy(s[j].password,
+			temp_password1);
+		strcpy(s[j].email, temp_email);
+		strcpy(s[j].mobile, temp_mobile);
+		j++;
+		printf("\n\n\nAccount"
+			" Successfully");
+		printf("Created!!\n\n");
+	}
+}
